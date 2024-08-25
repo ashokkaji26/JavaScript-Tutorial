d66af8e83b79ac24f3ed20e30b4dc63dda69e8b8
Ashok kaji <ashokkaji77@gmail.com>
2024-08-25 16:46:05 +0530

    updated Js notes

--- JS Codes/app.js
+++ JS Codes/app.js
@@ -1,10 +1,25 @@
-console.log("Hello World!");
-let a = 5;
-let b = 10;
-console.log("sum is:", a + b);
+// console.log("Hello World!");
+// let a = 5;
+// let b = 10;
+// console.log("sum is:", a + b);
 
-let pencilPrice = 10;   let penPrice = 20;
-console.log("The total price is: ", penPrice + pencilPrice, "Rupees.");
-let output = "The total price is: " + (penPrice + pencilPrice) + "Rupees.";
-console.log(` The total price is: ${penPrice + pencilPrice}     Rupees.`);
-console.log(output);
+// let pencilPrice = 10;   let penPrice = 20;
+// console.log("The total price is: ", penPrice + pencilPrice, "Rupees.");
+// let output = "The total price is: " + (penPrice + pencilPrice) + "Rupees.";
+// console.log(` The total price is: ${penPrice + pencilPrice}     Rupees.`);
+// console.log(output);
+
+let size = "XL";
+
+if(size ==="XL"){
+    console.log("Price is Rs. 250");
+}
+else if(size ==="L"){
+    console.log("Price is Rs. 200");
+}
+else if(size ==="M"){
+    console.log("Price is Rs. 100");
+}
+else{
+    console.log("Price is Rs. 50");
+}
---
+++ JS Codes/practice.js
@@ -0,0 +1,7 @@
+let color_r = red;
+let color_g = green;
+let color_y = yellow;
+
+if(color_r==red){
+    console.log("Stop");
+}
