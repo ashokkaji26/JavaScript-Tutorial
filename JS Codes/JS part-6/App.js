5cd05d3e78c9cc146e38b7b186f2aa513da16493
Ashok kaji <ashokkaji77@gmail.com>
2024-09-03 22:26:33 +0530

    Added JS-6

--- JS Codes/JS part-6/App.js
+++ JS Codes/JS part-6/App.js
@@ -1,6 +1,6 @@
 
 //----------------------------------------------------- Q1. --------------------------------------------------------------------------
-// Create a function that returns the sum of numbers from 1 to n.
+//Q. Create a function that returns the sum of numbers from 1 to n.
 // let num = prompt("Enter a number.");
 // let sum = 0;
 
@@ -14,18 +14,91 @@
 
 // console.log(sum);
 
-//----------------------------------------------------- Q2. ------------------------------------------------------------------------
-// Create a funtion that returns the concatenation of all strings in an array.
-let str = ["hi", "hello", "bye", "!"];
-let result = "";
 
-function concat(str) {
+//----------------------------------------------------- Q2. ------------------------------------------------------------------------
+//Q. Create a funtion that returns the concatenation of all strings in an array.
+// let str = ["hi", "hello", "bye", "!"];
+// let result = "";
+
+// function concat(str) {
  
-    for(let i=0; i<str.length; i++){
-        result += str[i];
+//     for(let i=0; i<str.length; i++){
+//         result += str[i];
+//     }
+//     return result;
+// }
+
+// concat(str);
+// console.log(result);
+
+
+//----------------------------------------------------- Q3. --------------------------------------------------------------------------
+//Q. Guess the output of below code.
+
+// let greet = "hello";
+
+// function outerGreet() {
+//     let greet = "Namaste";
+//     console.log(greet);
+
+//     function innerGreet() {
+//         console.log(greet);
+//     }
+// }
+
+// console.log(greet);
+// outerGreet();
+
+
+//----------------------------------------------------- Q4. --------------------------------------------------------------------------
+//Q. Create a function expression and change its value.
+
+// let sum = function(a,b) {
+//     return a+b;
+// }
+
+// sum = function(){
+//     console.log("hlw");
+// }
+// console.log(sum);
+
+
+
+//----------------------------------------------------- Q5. --------------------------------------------------------------------------
+//Q. Create a higher order function and call it.
+
+// function multipleGreet(func, count){     // higher order function.
+//     for(let i = 1; i<=count; i++){
+//         func();
+//     }
+// }
+
+// let greet = function(){           // fucntion expression.
+//     console.log("hello");
+// }
+
+// multipleGreet(greet, 10);       // function calling.
+
+
+//----------------------------------------------------- Q6. --------------------------------------------------------------------------
+//Q. Create a higher oder function which returns a function and call returned function.
+function oddOrEvenFactory(request) {
+    if(request == "odd"){
+        return function(n) {
+            console.log(!(n%2==0));
+        }
     }
-    return result;
+    else if(request == "even"){
+        let even = function(n){
+            console.log(n%2==0);
+        }
+        return even;
+    }
+    else{
+        console.log("Wrong request");
+    }
 }
 
-concat(str);
-console.log(result);
+let request = "odd";
+let odd = oddOrEvenFactory(request);
+odd(9);
